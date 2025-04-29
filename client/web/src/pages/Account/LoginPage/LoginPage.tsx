fdceb3903d7b2134cdfe7b4bec5566243acba801
ebeleey <ebeleey@gmail.com>
2025-04-29 17:45:55 +0900

    [FE] design: 로그인 화면 UI 구현

--- client/web/src/pages/Account/LoginPage/LoginPage.tsx
+++ client/web/src/pages/Account/LoginPage/LoginPage.tsx
@@ -1,10 +1,26 @@
-import InputField from '@/components/account/InputField'
+import Divider from '@/components/account/Divider'
+import KakaoLogin from './KakaoLogin'
+import NaverLogin from './NaverLogin'
+import ServiceLogin from './ServiceLogin'
 
 const LoginPage = () => {
   return (
-    <div>
-      <InputField label="아이디" placeholder="아이디를 입력하세요." />
-      <InputField label="비밀번호" placeholder="비밀번호를 입력하세요." />
+    <div className="flex flex-col items-center justify-center my-11">
+      <div className="w-full max-w-sm space-y-4">
+        <ServiceLogin />
+        <Divider text="OR" />
+        <KakaoLogin />
+        <NaverLogin />
+
+        <div className="flex justify-center text-sm text-gray-500">
+          <p>
+            회원이 아니신가요?{' '}
+            <a href="/account/signup" className="hover:underline">
+              회원가입
+            </a>
+          </p>
+        </div>
+      </div>
     </div>
   )
 }
---
+++ client/web/src/pages/Account/LoginPage/NaverLogin.tsx
@@ -0,0 +1,28 @@
+import { Button } from '@/components/ui/button'
+
+const NaverLogin = () => {
+  return (
+    <Button
+      className="w-full h-12 rounded-lg flex items-center justify-center bg-[#03C75A] hover:bg-[#02b350] text-white font-medium"
+      onClick={() => console.log('네이버 로그인')}
+    >
+      <div className="flex items-center">
+        <svg
+          width="24"
+          height="24"
+          viewBox="0 0 24 24"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            d="M14.9 12.612L9.1 4H4V20H9.1V11.388L14.9 20H20V4H14.9V12.612Z"
+            fill="white"
+          />
+        </svg>
+        <span className="ml-2">네이버 계정으로 로그인</span>
+      </div>
+    </Button>
+  )
+}
+
+export default NaverLogin
