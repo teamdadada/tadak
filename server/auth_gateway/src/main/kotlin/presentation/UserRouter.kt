e3dbec88f3963380f68c0668578a26c96fa7f346
배승호 <moomu.like@gmail.com>
2025-05-01 17:25:37 +0900

    [AUTH] feat: 프로필이미지 변경 기능

--- server/auth_gateway/src/main/kotlin/presentation/UserRouter.kt
+++ server/auth_gateway/src/main/kotlin/presentation/UserRouter.kt
@@ -1,5 +1,6 @@
 package com.tadak.presentation
 
+import com.tadak.application.ImageUploadService
 import com.tadak.domain.entity.User
 import com.tadak.domain.table.Users
 import com.tadak.dto.AuthenticatedUserKey
@@ -14,12 +15,14 @@ import com.tadak.exception.status.ForbiddenException
 import com.tadak.exception.status.NotFoundException
 import com.tadak.util.PasswordUtil
 import io.ktor.http.*
+import io.ktor.http.content.*
 import io.ktor.server.request.*
 import io.ktor.server.response.respond
 import io.ktor.server.routing.*
 import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.transactions.transaction
 import org.jetbrains.exposed.sql.SqlExpressionBuilder.eq
+import java.time.LocalDateTime
 
 fun Route.userRoutes() {
     route("/user") {
@@ -63,6 +66,27 @@ fun Route.userRoutes() {
             user?.let {
                 call.respond(UserSelectResponse.from(user))
             } ?: throw NotFoundException(UserErrorCode.USER_NOT_FOUND.toErrorCode())
+        }
+
+        post("/img") {
+            val authUser = call.attributes.getOrNull(AuthenticatedUserKey)
+                ?: throw BadRequestException(AuthErrorCode.UNAUTHORIZED_REQUEST.toErrorCode())
+
+            val multipart = call.receiveMultipart()
+            val filePart = multipart.readPart() as? PartData.FileItem
+                ?: throw BadRequestException(UserErrorCode.USER_BAD_REQUEST.toErrorCode())
+
+            val imageUrl = ImageUploadService.uploadImage(filePart)
+
+            transaction {
+                val user = User.find { Users.id eq authUser.userUuid }.singleOrNull()
+                    ?: throw NotFoundException(UserErrorCode.USER_NOT_FOUND.toErrorCode())
+                user.profileImg = imageUrl
+                user.updatedAt = LocalDateTime.now()
+            }
+
+            call.response.headers.append(HttpHeaders.Location, imageUrl)
+            call.respond(HttpStatusCode.Created)
         }
     }
 
