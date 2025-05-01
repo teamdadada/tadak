472fc93959970a1ac236bd6f3d81736ca7437461
박민경 <hogik1027@naver.com>
2025-05-02 06:05:12 +0900

    [S12P31A703-252] [FE] chore: 임시 endPoints 정의

--- client/web/src/services/endPoints.ts
+++ client/web/src/services/endPoints.ts
@@ -1,24 +1,31 @@
 // userService
 // 예시로 작성해 둔 것이라 필요에 따라 수정 가능
 export const USER_END_POINT = {
-    SOCIAL_LOGIN: (socialType: string, code: string) =>
-        `/auth/login/${socialType}?code=${code}`,
-    LOGOUT: '/auth/logout',
-    GET_USER_INFO: '/users',
-    UPDATE_EMAIL: '/users',
-    SEND_EMAIL_CODE: '/users/emails/send',
-    VERIFY_EMAIL_CODE: (code: string) => `/users/emails/verify?code=${code}`,
-    DELETE_USER: '/users',
+  SOCIAL_LOGIN: (socialType: string, code: string) =>
+    `/auth/login/${socialType}?code=${code}`,
+  LOGOUT: '/auth/logout',
+  GET_USER_INFO: '/users',
+  UPDATE_EMAIL: '/users',
+  SEND_EMAIL_CODE: '/users/emails/send',
+  VERIFY_EMAIL_CODE: (code: string) => `/users/emails/verify?code=${code}`,
+  DELETE_USER: '/users',
 }
 
 // mainService
 // 예시로 작성해 둔 것이라 필요에 따라 수정 가능
-export const MAIN_END_POINT = {
-
-}
+export const MAIN_END_POINT = {}
 
 // shopService
 // 예시로 작성해 둔 것이라 필요에 따라 수정 가능
 export const SHOP_END_POINT = {
-
+  FILTER: {
+    BEARBONE: '/filters/bearbone',
+    SWITCH: '/filters/switch',
+    KEYCAP: '/filters/keycap',
+  },
+  PRODUCT: {
+    BEARBONE: '/products/bearbone',
+    SWITCH: '/products/switch',
+    KEYCAP: '/products/keycap',
+  },
 }
