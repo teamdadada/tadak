c2ef6929ca45ad0d89557a2ca6cb8cfa70577fb8
shoomon <kate010117@naver.com>
2025-05-02 02:15:09 +0900

    [BE] comment: 필요없는 주석 제거

--- server/fastapi/src/chatbot/util/embedding_util.py
+++ server/fastapi/src/chatbot/util/embedding_util.py
@@ -2,13 +2,10 @@ import os
 from typing import List
 from fastapi import UploadFile
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_upstage import UpstageEmbeddings, UpstageDocumentParseLoader
 from langchain_community.vectorstores import Qdrant
 from langchain_huggingface import HuggingFaceEmbeddings
 import src.common.config.qdrant_config as qdrant_config
 from langchain_core.documents import Document
-
-# UPSTAGE_API_KEY = os.getenv("UPSTAGE_API_KEY")
 
 # todo: documentparser에서 upstage api 제거
 async def load_documents(files: List[UploadFile], file_detail: str):
@@ -49,10 +46,6 @@ def get_docs_split_list(docs):
 
 
 def embed_documents(docs):
-    # embeddings = UpstageEmbeddings(
-    #     api_key=UPSTAGE_API_KEY,
-    #     model="solar-embedding-1-large"
-    # )
     embeddings = HuggingFaceEmbeddings(model_name='jhgan/ko-sroberta-multitask')
 
     Qdrant.from_documents(
