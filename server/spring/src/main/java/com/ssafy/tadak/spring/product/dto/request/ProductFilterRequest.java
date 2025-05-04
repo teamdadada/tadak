8274ddd758a708d994fb2139f043730a1519b8aa
jjw05015 <jjw05015@naver.com>
2025-05-05 01:32:02 +0900

    [BE] refactor: 사용하지 않는 request 제거

--- server/spring/src/main/java/com/ssafy/tadak/spring/product/dto/request/ProductFilterRequest.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.tadak.spring.product.dto.request;
-
-import com.ssafy.tadak.spring.product.util.enums.ProductType;
-
-public record ProductFilterRequest(
-        ProductType productType
-) {
-}
