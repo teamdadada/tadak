c1f6cb82670160508fc63536bf2c04af508e561c
박민경 <hogik1027@naver.com>
2025-05-07 17:32:42 +0900

    [S12P31A703-252] [FE] chore: endPoints 정보 업데이트

--- client/web/src/services/endPoints.ts
+++ client/web/src/services/endPoints.ts
@@ -1,41 +1,43 @@
 // userService
 // 예시로 작성해 둔 것이라 필요에 따라 수정 가능
 export const USER_END_POINT = {
-    SOCIAL_LOGIN: (socialType: string, code: string) =>
-        `/auth/login/${socialType}?code=${code}`,
-    LOGOUT: '/auth/logout',
-    GET_USER_INFO: '/users',
-    UPDATE_EMAIL: '/users',
-    SEND_EMAIL_CODE: '/users/emails/send',
-    VERIFY_EMAIL_CODE: (code: string) => `/users/emails/verify?code=${code}`,
-    DELETE_USER: '/users',
+  SIGNUP: '/user/signup',
+
+  SOCIAL_LOGIN: (socialType: string, code: string) =>
+    `/auth/login/${socialType}?code=${code}`,
+  LOGOUT: '/auth/logout',
+  GET_USER_INFO: '/users',
+  UPDATE_EMAIL: '/users',
+  SEND_EMAIL_CODE: '/users/emails/send',
+  VERIFY_EMAIL_CODE: (code: string) => `/users/emails/verify?code=${code}`,
+  DELETE_USER: '/users',
 }
 
 export const AUTH_END_POINT = {
   LOGIN: '/auth/login',
   CHECK: '/auth/check',
-  REISSUE: '/auth/reissue'
+  REISSUE: '/auth/reissue',
 }
 
 // mainService
 export const MAIN_END_POINT = {
-  POPULAR_ITEMS: (productType: string) =>
-    `/api/product/${productType}/popular`,
-  LATEST_ITEMS: (productType: string) =>
-    `/api/product/${productType}/latest`,
+  POPULAR_ITEMS: (productType: string) => `/api/product/${productType}/popular`,
+  LATEST_ITEMS: (productType: string) => `/api/product/${productType}/latest`,
 }
 
 // shopService
 // 예시로 작성해 둔 것이라 필요에 따라 수정 가능
 export const SHOP_END_POINT = {
   FILTER: {
-    BEARBONE: '/filters/bearbone',
-    SWITCH: '/filters/switch',
-    KEYCAP: '/filters/keycap',
+    BEARBONE: '/api/product/BAREBONE/filter',
+    SWITCH: '/api/product/SWITCH/filter',
+    KEYCAP: '/api/product/KEYCAP/filter',
   },
   PRODUCT: {
-    BEARBONE: '/products/bearbone',
-    SWITCH: '/products/switch',
-    KEYCAP: '/products/keycap',
+    BEARBONE: '/api/product/BAREBONE',
+    SWITCH: '/api/product/SWITCH',
+    KEYCAP: '/api/product/KEYCAP',
+    LATEST: (type: string) => `/api/product/${type}/latest`,
+    POPULAR: (type: string) => `/api/product/${type}/popular`,
   },
 }
