9cbcb7e8985f23a2b08e240b666f311bd6bd6632
박민경 <hogik1027@naver.com>
2025-05-08 09:13:43 +0900

    [S12P31A703-252] [FE] fix: 빌드 오류 수정

--- client/web/src/components/shop/AsideRecommendation.tsx
+++ client/web/src/components/shop/AsideRecommendation.tsx
@@ -1,23 +1,23 @@
-import ItemCard from "./ItemCard"
+import ItemCard from './ItemCard'
 
 const recommendedItems = [
   {
     id: 1,
-    name: "타닥 베어본 키보드",
+    name: '타닥 베어본 키보드',
     price: 129000,
-    imageUrl: "https://cataas.com/cat?1",
+    imageUrl: 'https://cataas.com/cat?1',
   },
   {
     id: 2,
-    name: "스무스 리니어 스위치",
+    name: '스무스 리니어 스위치',
     price: 39000,
-    imageUrl: "https://cataas.com/cat?2",
+    imageUrl: 'https://cataas.com/cat?2',
   },
   {
     id: 3,
-    name: "클래식 키캡 세트",
+    name: '클래식 키캡 세트',
     price: 49000,
-    imageUrl: "https://cataas.com/cat?3",
+    imageUrl: 'https://cataas.com/cat?3',
   },
 ]
 
@@ -29,12 +29,12 @@ const AsideRecommendation = () => {
         {recommendedItems.map((item) => (
           <li key={item.id} className="flex justify-center">
             <ItemCard
+              id={item.id}
               name={item.name}
               price={item.price}
               imageUrl={item.imageUrl}
               size="sm" // 작은 사이즈로 제한
               liked={false} // 기본 비활성화
-              to={`/products/${item.id}`}
             />
           </li>
         ))}
