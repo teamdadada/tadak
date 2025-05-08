9f49232ecbbf6a5def8cd69392b52fd0d49d4eba
TaeYeongKIM <taeyeong0114@gmail.com>
2025-05-08 16:07:29 +0900

    [FE] chore: API baseURL 수정

--- client/web/src/services/http-common.ts
+++ client/web/src/services/http-common.ts
@@ -4,10 +4,10 @@ import { useAuthStore } from '@/store/authStore'
 import { toast } from 'sonner'
 
 // 환경에 따른 baseURL 설정
-const BASE_URL = import.meta.env.VITE_API_BASE_URL
+// const BASE_URL = import.meta.env.VITE_API_BASE_URL
 
 const http = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://dapi.tadak.kr',
   headers: {
     'Content-Type': 'application/json',
   },
@@ -15,7 +15,7 @@ const http = axios.create({
 })
 
 export const refreshHttp = axios.create({
-  baseURL: BASE_URL,
+  baseURL: 'https://dapi.tadak.kr',
   headers: {
     'Content-Type': 'application/json',
   },
