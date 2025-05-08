c1088d3ed2ab6f652d3c56683c22514007aefcac
shoomon <kate010117@naver.com>
2025-05-08 17:47:18 +0900

    [BE] chore: 팔요 없는 패키지 삭제

--- server/spring/src/main/java/com/ssafy/tadak/spring/payment/service/PaymentService.java
+++ server/spring/src/main/java/com/ssafy/tadak/spring/payment/service/PaymentService.java
@@ -1,6 +1,5 @@
 package com.ssafy.tadak.spring.payment.service;
 
-import com.fasterxml.jackson.core.JsonParseException;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ssafy.tadak.spring.payment.dto.PaymentCustomData;
