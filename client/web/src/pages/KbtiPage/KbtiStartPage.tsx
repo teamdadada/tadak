77e6f9e03e73fac3fa10d2a68a76f713df037c13
박민경 <hogik1027@naver.com>
2025-05-09 16:26:06 +0900

    [S12P31A703-396] [FE] feat: KBTI 시작 페이지 UI 구현 및 시작 버튼으로 검사 페이지 이동 기능 추가

--- client/web/src/pages/KbtiPage/KbtiStartPage.tsx
+++ client/web/src/pages/KbtiPage/KbtiStartPage.tsx
@@ -1,11 +1,26 @@
-import { Outlet } from 'react-router-dom'
+import KbtiStartImage from '@/assets/images/KBTI_start.svg'
+import { Button } from '@/components/ui/button'
+import { useNavigate } from 'react-router-dom'
 
 const KbtiStartPage = () => {
+  const navigate = useNavigate()
+
+  const handleStartTest = () => {
+    navigate('/kbti/test')
+  }
+
   return (
-    <div className="p-4">
-      <h1 className="text-2xl font-bold">🔍 KBTI 테스트 페이지</h1>
-      <p>당신의 키보드 성향을 알아보세요!</p>
-      <Outlet />
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-68px)] gap-8 p-4">
+      <img
+        src={KbtiStartImage}
+        alt="KBTI 시작 이미지"
+        className="w-full p-4 max-h-[70vh]"
+      />
+      <div className="flex justify-center w-full">
+        <Button size="lg" onClick={handleStartTest}>
+          테스트 해보기
+        </Button>
+      </div>
     </div>
   )
 }
