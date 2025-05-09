59c81aaf5dabe9a4d2b822814d6a73725dcc771d
박민경 <hogik1027@naver.com>
2025-05-09 17:06:31 +0900

    [S12P31A703-396] [FE] design: KBTI 시작 페이지 스타일 변경

--- client/web/src/pages/KbtiPage/KbtiStartPage.tsx
+++ client/web/src/pages/KbtiPage/KbtiStartPage.tsx
@@ -10,14 +10,17 @@ const KbtiStartPage = () => {
   }
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-68px)] gap-8 p-4">
-      <img
-        src={KbtiStartImage}
-        alt="KBTI 시작 이미지"
-        className="w-full p-4 max-h-[70vh]"
-      />
-      <div className="flex justify-center w-full">
-        <Button size="lg" onClick={handleStartTest}>
+    <div className="flex flex-col items-center justify-center min-h-[calc(100vh-68px)] gap-4 px-4 bg-[rgb(254,247,230)]">
+      <div className="flex flex-col justify-center w-full p-2">
+        {/* <h1 className="w-full p-4 text-2xl font-bold">🔍 KBTI</h1> */}
+        <img src={KbtiStartImage} alt="KBTI 시작 이미지" />
+      </div>
+      <div className="flex justify-center w-full p-4 mt-4">
+        <Button
+          size="lg"
+          className="bg-tadak-primary"
+          onClick={handleStartTest}
+        >
           테스트 해보기
         </Button>
       </div>
