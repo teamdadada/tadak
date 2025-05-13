91512e66a9cbd31b44963a5b1d46bfddbd6c89ef
배승호 <moomu.like@gmail.com>
2025-05-13 11:18:29 +0900

    [BE] feat: 유저 정보 수정 API

--- server/auth_gateway/src/main/kotlin/exception/error_code/UserErrorCode.kt
+++ server/auth_gateway/src/main/kotlin/exception/error_code/UserErrorCode.kt
@@ -9,6 +9,9 @@ enum class UserErrorCode(
     // 400
     USER_BAD_REQUEST("U4000", "요청을 처리하는데 필요한 인자가 NULL을 참조합니다."),
 
+    // 401
+    INVALID_PASSWORD_EXCEPTION("U4010", "기존 비밀번호가 일치하지 않습니다."),
+
     // 404
     USER_NOT_FOUND("U4040", "해당하는 유저를 찾을 수 없습니다."),
 
