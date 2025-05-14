db8147ec25f8bda27c586645cc814383c693ad98
ebeleey <ebeleey@gmail.com>
2025-05-14 18:45:57 +0900

    [FE] feat: API 호출 훅 작성

--- client/web/src/services/zzimService.ts
+++ client/web/src/services/zzimService.ts
@@ -1,5 +1,6 @@
-import { ZZIM_END_POINT } from './endPoints'
 import http from './http-common'
+
+import { ZZIM_END_POINT } from './endPoints'
 
 export const addZzim = async (productId: number) => {
   const response = await http.post(ZZIM_END_POINT.ADD(productId))
