848f3666ffa949f9905bb107c744242066deca4c
ebeleey <ebeleey@gmail.com>
2025-05-14 17:53:13 +0900

    [FE] design: 마이페이지 찜목록 UI 구현 및 ItemCard 디자인 수정

--- client/web/src/components/shop/ItemCard.tsx
+++ client/web/src/components/shop/ItemCard.tsx
@@ -30,28 +30,28 @@ const ItemCard = ({
         if (type) navigate(`/product/${type}/${productId}`)
       }}
       className={cn(
-        'border rounded-lg p-4 flex flex-col justify-between items-center relative bg-white',
+        'rounded-lg p-5 flex flex-col justify-between items-center relative bg-tadak-white cursor-pointer',
         sizeClasses[size],
       )}
     >
-      <div className="relative flex items-center justify-center w-full h-full overflow-hidden rounded-md">
+      <div className="relative flex items-center justify-center w-full h-full overflow-hidden ">
         <img
           src={thumbnail}
           alt={name}
           className="object-cover w-full h-full"
         />
-        <div className="absolute p-1 rounded-lg top-2 right-2 bg-tadak-white/80">
+        <div className="absolute p-1 rounded-lg top-2 right-2">
           {liked ? (
-            <FaHeart className="w-5 h-5 text-red-500" />
+            <FaHeart className="w-5 h-5 text-tadak-warning" />
           ) : (
-            <FaRegHeart className="w-5 h-5 text-red-500" />
+            <FaRegHeart className="w-5 h-5 text-tadak-white drop-shadow-md" />
           )}
         </div>
       </div>
 
       <div className="flex flex-col justify-center w-full h-16 mt-4 text-left">
-        <div className="font-semibold">{name}</div>
-        <div className="mt-1">
+        <div className="text-[15px] ">{name}</div>
+        <div className="mt-1 text-[15px] font-semibold">
           {minPrice !== undefined
             ? `${minPrice.toLocaleString()}원`
             : '가격 정보 없음'}
--- client/web/src/pages/MyPage/UserLikes.tsx
+++ client/web/src/pages/MyPage/UserLikes.tsx
@@ -1,7 +1,40 @@
+import ItemCard from '@/components/shop/ItemCard'
+import Tabs from '@/components/ui/Tabs'
+
+const tabLabels = ['전체', '베어본', '스위치', '키캡']
+
 const UserLikes = () => {
+  const exampleProduct = {
+    productId: 1,
+    name: '어쩌고저쩌고',
+    minPrice: 30,
+    thumbnail:
+      'https://minio.tadak.kr/profile/profile_image_81d46d76-012a-4e30-8045-dca585dd9ada.jpg',
+  }
   return (
     <div>
-      <h1>찜</h1>
+      <Tabs
+        items={tabLabels}
+        // selectedIndex={selectedIndex}
+        // onChange={onChange}
+        tabWidth={100}
+        indicatorWidth={100}
+        indicatorHeight={2}
+        tabClassName="text-base"
+      />
+      <div className="grid grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mb-8">
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+        <ItemCard {...exampleProduct} />
+      </div>
     </div>
   )
 }
