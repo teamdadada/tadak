555d97b6da1d2b29a9ecc40ec2c5f13a3ef557aa
ebeleey <ebeleey@gmail.com>
2025-05-14 10:54:23 +0900

    [FE] feat: 프로필 수정 훅 작성

--- client/web/src/hooks/useUser.ts
+++ client/web/src/hooks/useUser.ts
@@ -1,8 +1,20 @@
 import { signIn } from '@/services/authService'
-import { getUserInfo, signUp } from '@/services/userService'
-import { ErrorResponse, SignUpRequest, User } from '@/types/user'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import {
+  getUserInfo,
+  signUp,
+  updateNickname,
+  updateProfileImg,
+} from '@/services/userService'
+import {
+  ErrorResponse,
+  SignUpRequest,
+  UpdateNicknameRequest,
+  UpdateProfileImgRequest,
+  User,
+} from '@/types/user'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
+import { useNavigate } from 'react-router-dom'
 import { toast } from 'sonner'
 
 export const useSignUp = () => {
@@ -40,4 +52,60 @@ export const useGetUserInfo = () => {
     queryKey: ['user', 'me'],
     queryFn: getUserInfo,
   })
+}
+
+export const useUpdateNickname = () => {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  const { mutateAsync } = useMutation({
+    mutationFn: (data: UpdateNicknameRequest) => updateNickname(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user', 'me'] })
+      toast.success('닉네임이 수정되었습니다!')
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const status = error.response?.status
+      const code = error.response?.data?.code
+      const message = error.response?.data?.message
+
+      if (status == 401 && code === 'B4011') {
+        navigate('/account/login', { replace: true })
+        toast.error(message) // 로그인되지 않은 사용자입니다.
+      } else if (status == 404 && code === 'U4040') {
+        navigate('/account/login', { replace: true })
+        toast.error(message) // 해당하는 유저를 찾을 수 없습니다.
+      }
+    },
+  })
+  return mutateAsync
+}
+
+export const useUpdateProfileImg = () => {
+  const queryClient = useQueryClient()
+  const navigate = useNavigate()
+
+  const { mutateAsync } = useMutation({
+    mutationFn: (data: UpdateProfileImgRequest) => updateProfileImg(data),
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['user', 'me'] })
+      toast.success('프로필 이미지가 변경되었습니다!')
+    },
+    onError: (error: AxiosError<ErrorResponse>) => {
+      const status = error.response?.status
+      const code = error.response?.data?.code
+      const message = error.response?.data?.message
+
+      if (status === 400 && code === 'U4000') {
+        toast.error('프로필 이미지를 다시 업로드해주세요.')
+      } else if (status == 401 && code === 'B4011') {
+        navigate('/account/login', { replace: true })
+        toast.error(message) // 로그인되지 않은 사용자입니다.
+      } else if (status == 404 && code === 'U4040') {
+        navigate('/account/login', { replace: true })
+        toast.error(message) // 해당하는 유저를 찾을 수 없습니다.
+      }
+    },
+  })
+  return mutateAsync
 }
