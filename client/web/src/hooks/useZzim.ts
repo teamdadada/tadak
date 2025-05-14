769d9bcf0a99b6afff4dc1a95c8d2655b1166291
ebeleey <ebeleey@gmail.com>
2025-05-14 19:09:04 +0900

    [FE] feat: 찜 등록 시 찜 목록 리페치 기능 구현

--- client/web/src/hooks/useZzim.ts
+++ client/web/src/hooks/useZzim.ts
@@ -1,14 +1,17 @@
 import { addZzim, listZzim } from '@/services/zzimService'
 import { ErrorResponse } from '@/types/user'
 import { ZzimListResponse } from '@/types/zzim'
-import { useMutation, useQuery } from '@tanstack/react-query'
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
 import { AxiosError } from 'axios'
 import { toast } from 'sonner'
 
 export const useAddZzim = () => {
+  const queryClient = useQueryClient()
   const { mutateAsync } = useMutation({
     mutationFn: addZzim,
-    onSuccess: () => {},
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ['zzim'] })
+    },
     onError: (error: AxiosError<ErrorResponse>) => {
       const message = error.response?.data?.message
       toast.error(message)
