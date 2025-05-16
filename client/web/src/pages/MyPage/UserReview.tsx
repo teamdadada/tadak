ce2e65caec9df1f0b6407e6f4c84b5f3571cb4f5
박민경 <hogik1027@naver.com>
2025-05-16 14:38:41 +0900

    [FE] feat: 마이페이지에서 내 리뷰 목록 조회 기능 구현

--- client/web/src/pages/MyPage/UserReview.tsx
+++ client/web/src/pages/MyPage/UserReview.tsx
@@ -1,7 +1,32 @@
+import ProductSummaryItem from '@/components/product/ProductSummaryItem'
+import ReviewItem from '@/components/review/ReviewItem'
+import { getMyReviews } from '@/services/reviewService'
+import { useQuery } from '@tanstack/react-query'
+
 const UserReview = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['myReviews'],
+    queryFn: getMyReviews,
+  })
+
+  if (isLoading) return <div>로딩 중...</div>
+  if (isError || !data) return <p>에러 발생</p>
+
   return (
-    <div>
-      <h1>작성한 리뷰</h1>
+    <div className="flex flex-col gap-6">
+      {data?.reviews.map((review) => (
+        <div className="flex flex-col gap-2 border-b border-tadak-light-gray">
+          <ProductSummaryItem
+            name={review.product.name}
+            thumbnail={review.product.thumbnail}
+          />
+          <ReviewItem
+            key={review.reviewId}
+            review={review}
+            productId={review.product.productId}
+          />
+        </div>
+      ))}
     </div>
   )
 }
