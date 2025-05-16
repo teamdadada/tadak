1bba709099571e48f76df8c355af7fc9e7630e40
shoomon <kate010117@naver.com>
2025-05-16 10:39:45 +0900

    [BE] feat: 배치정보 엔티티에 키보드 아이디 추가

--- server/spring/src/main/java/com/ssafy/tadak/spring/placement/domain/entity/Background.java
+++ server/spring/src/main/java/com/ssafy/tadak/spring/placement/domain/entity/Background.java
@@ -1,11 +1,15 @@
 package com.ssafy.tadak.spring.placement.domain.entity;
 
+import com.ssafy.tadak.spring.keyboard.domain.entity.Keyboard;
 import com.ssafy.tadak.spring.minio.domain.entity.Image;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
@@ -32,6 +36,10 @@ public class Background {
     @Column(name = "user_id")
     private Long userId;
 
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "keyboard_id")
+    private Keyboard keyboard;
+
     @OneToOne
     @Column(name = "image_id")
     Image image;
@@ -53,6 +61,8 @@ public class Background {
 
     @Column(name = "rotation_Z")
     Double rotationZ;
+
+    // todo: 크기
 
     @Column(name = "created_at")
     private LocalDateTime createdAt;
