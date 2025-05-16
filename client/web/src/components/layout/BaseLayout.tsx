ac6560d8aac35dc76e59c96985c57a61a8010888
ebeleey <ebeleey@gmail.com>
2025-05-16 15:19:37 +0900

    [FE] feat: 페이지 전환 시 스크롤 위치 자동 복원 기능 추가

--- client/web/src/components/layout/BaseLayout.tsx
+++ client/web/src/components/layout/BaseLayout.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from 'react-router-dom'
 import Header from '@/components/layout/Header'
+import ScrollToTop from '../common/ScrollToTop'
 
 const BaseLayout = () => {
   return (
     <div className="flex flex-col w-full h-full min-h-screen">
+      <ScrollToTop />
       <Header />
       <main className="flex-1">
         <Outlet />
