f212ca6c2a071dea6d64d32385e541c8c61ed666
ebeleey <ebeleey@gmail.com>
2025-05-19 17:46:26 +0900

    [FE] design: 서비스 준비 중 UI 추가

--- client/web/src/pages/MyPage/UserShoppingHistory.tsx
+++ client/web/src/pages/MyPage/UserShoppingHistory.tsx
@@ -1,7 +1,18 @@
+import { Receipt } from 'lucide-react'
+
 const UserShoppingHistory = () => {
   return (
-    <div>
-      <h1>결제 내역</h1>
+    <div className="w-full max-w-4xl mx-auto">
+      {/* 결제 내역 영역 */}
+      <div className=" flex flex-col items-center justify-center min-h-[400px]">
+        <Receipt className="w-16 h-16 text-tadak-primary mx-auto mb-4" />
+        <h2 className="text-xl font-bold mb-2 text-tadak-black">
+          서비스 준비 중
+        </h2>
+        <p className="text-tadak-black mb-6">
+          주문 및 결제 내역을 확인할 수 있는 기능이 곧 제공될 예정입니다.
+        </p>
+      </div>
     </div>
   )
 }
