108c22994a615e3c202a9605feb5e9c13f9b153e
ebeleey <ebeleey@gmail.com>
2025-05-19 18:01:05 +0900

    [FE] design: 인사하는 타덕으로 변경

--- client/web/src/pages/MyPage/UserShoppingHistory.tsx
+++ client/web/src/pages/MyPage/UserShoppingHistory.tsx
@@ -1,11 +1,11 @@
-import { Receipt } from 'lucide-react'
+import SorryTaduck from '@/assets/images/sorry.png'
 
 const UserShoppingHistory = () => {
   return (
     <div className="w-full max-w-4xl mx-auto">
       {/* 결제 내역 영역 */}
       <div className=" flex flex-col items-center justify-center min-h-[400px]">
-        <Receipt className="w-16 h-16 text-tadak-primary mx-auto mb-4" />
+        <img src={SorryTaduck} alt="인사타덕" className="w-32 h-32 mb-4" />
         <h2 className="text-xl font-bold mb-2 text-tadak-black">
           서비스 준비 중
         </h2>
