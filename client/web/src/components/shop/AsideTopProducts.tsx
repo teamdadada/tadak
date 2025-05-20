b5ae4f2c696da56971ff950a2423d31b26a3fb24
박민경 <hogik1027@naver.com>
2025-05-20 16:13:41 +0900

    [S12P31A703-482] [FE] feat: 사이드바 인기상품 API 연동

--- client/web/src/components/shop/AsideTopProducts.tsx
+++ client/web/src/components/shop/AsideTopProducts.tsx
@@ -1,37 +1,42 @@
 import ItemCard from './ItemCard'
-
-const recommendedItems = [
-  {
-    productId: 1,
-    name: '타닥 베어본 키보드',
-    minPrice: 129000,
-    thumbnail: 'https://cataas.com/cat?1',
-  },
-  {
-    productId: 2,
-    name: '스무스 리니어 스위치',
-    minPrice: 39000,
-    thumbnail: 'https://cataas.com/cat?2',
-  },
-  {
-    productId: 3,
-    name: '클래식 키캡 세트',
-    minPrice: 49000,
-    thumbnail: 'https://cataas.com/cat?3',
-  },
-]
+import { getTopProducts } from '@/services/shopService'
+import { useQuery } from '@tanstack/react-query'
 
 const AsideRecommendation = () => {
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['topProducts'],
+    queryFn: () => getTopProducts({ size: 5 }),
+  })
+
+  if (isLoading) return null
+
   return (
     <aside className="hidden p-4 text-sm rounded-md w-[180px] lg:block bg-tadak-light-gray text-tadak-dark-gray">
-      <h2 className="mb-2 font-semibold text-tadak-black">💡 인기상품품</h2>
-      <ul className="space-y-2">
-        {recommendedItems.map((item) => (
-          <li key={item.productId} className="flex justify-center">
-            <ItemCard {...item} size="sm" />
-          </li>
-        ))}
-      </ul>
+      <h2 className="mb-2 font-semibold text-tadak-black">💡 인기상품</h2>
+      {isLoading && (
+        <ul className="space-y-2">
+          {Array.from({ length: 5 }).map((_, idx) => (
+            <li
+              key={idx}
+              className="w-full h-[80px] bg-tadak-gray animate-pulse rounded-md"
+            />
+          ))}
+        </ul>
+      )}
+
+      {isError && (
+        <p className="text-sm font-medium text-tadak-warning">⚠ 에러 발생</p>
+      )}
+
+      {!isLoading && !isError && (
+        <ul className="space-y-2">
+          {data?.list.map((item) => (
+            <li key={item.productId} className="flex justify-center">
+              <ItemCard {...item} size="sm" />
+            </li>
+          ))}
+        </ul>
+      )}
     </aside>
   )
 }
