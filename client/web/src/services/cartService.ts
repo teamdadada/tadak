9a46ed6058817b7e0038dafd7b531cca6e36e559
박민경 <hogik1027@naver.com>
2025-05-21 10:29:54 +0900

    [S12P31A703-488] [FE] feat: 장바구니 API 함수 구현

--- client/web/src/services/cartService.ts
+++ client/web/src/services/cartService.ts
@@ -1,8 +1,14 @@
 import http from '@/services/http-common'
 import { CART_END_POINT } from './endPoints'
+import { cartItem } from '@/types/cart'
 
 // TODO: 장바구니에 키보드 담기
 export const addToCart = async (payload: any) => {
   const response = await http.post(CART_END_POINT.ADD, payload)
   return response.data
+}
+
+export const getCart = async (): Promise<cartItem[]> => {
+  const { data } = await http.get(CART_END_POINT.LIST)
+  return data
 }
